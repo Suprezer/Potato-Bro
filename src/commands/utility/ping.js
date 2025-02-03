888a9d5e841e82e1dbf44bcc97de2abd3f212343
Jonas M. Olesen <jmo@live.dk>
2025-02-04 00:00:19 +0100

    Basic bot functionality with commands

---
+++ src/commands/utility/ping.js
@@ -0,0 +1,14 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+// Exporting the command module
+module.exports = {
+    // Defining the command data using SlashCommandBuilder
+    data: new SlashCommandBuilder()
+        .setName('ping') // Setting the command name to 'ping'
+        .setDescription('Replies with Pong!'), // Setting the command description
+    // Defining the execute function that will run when the command is used
+    async execute(interaction) {
+        // Sending a reply to the interaction with the message 'Pong!'
+        await interaction.reply('Pong!');
+    },
+};
